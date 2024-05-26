c82b95978d236211c4220e8aa79ec1a24b1ab110
Kazyel <m.mascarelo@gmail.com>
2024-05-26 20:04:49 -0300

    Starting Own 'Auth'

--- restzin/src/index.ts
+++ restzin/src/index.ts
@@ -1,4 +1,6 @@
 import app from "./server";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
--- restzin/src/server.ts
+++ restzin/src/server.ts
@@ -1,14 +1,22 @@
 import express from "express";
 import router from "./router";
 import path from "path";
+import morgan from "morgan";
+import cors from "cors";
 import { Request, Response } from "express";
+import { protectRoute } from "./modules/auth";
 
 const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).sendFile(path.resolve("public/index.html"));
 });
 
-app.use("/api", router);
+app.use("/api", protectRoute, router);
 
 export default app;
