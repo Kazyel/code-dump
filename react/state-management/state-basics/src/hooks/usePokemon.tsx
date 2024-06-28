da2910d794b34c9ad0fcacacb005791ebf1ecd21
Kazyel <m.mascarelo@gmail.com>
2024-06-28 14:10:30 -0300

    removing desnecessary code

--- react/state-management/state-basics/src/hooks/usePokemon.tsx
+++ react/state-management/state-basics/src/hooks/usePokemon.tsx
@@ -4,9 +4,7 @@ export const usePokemon = () => {
     const [pokemon, setPokemon] = useState<string[]>([]);
 
     const handlePokemonChange = (pokemon: string) => {
-        if (pokemon !== null) {
-            setPokemon((pokemons) => [pokemon, ...pokemons]);
-        }
+        setPokemon((pokemons) => [pokemon, ...pokemons]);
     };
 
     return { pokemon, handlePokemonChange };
